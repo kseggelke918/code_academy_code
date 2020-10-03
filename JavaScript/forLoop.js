2a0ee9a3a04c09d317c7c0f2787bdb95042a1cfd
kseggelke918 <katie.seggelke@gmail.com>
2020-10-02 20:50:11 -0600

    add javascript folder, add forLoops.js

---
+++ JavaScript/forLoop.js
@@ -0,0 +1,15 @@
+for (let counter = 5 ; counter < 11 ; counter ++) {
+    console.log(counter)
+  }
+
+  // The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+for (let counter = 3; counter >= 0; counter--){
+    console.log(counter);
+  }
+
+  const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+for(let i = 0; i < vacationSpots.length; i++) {
+  console.log(`I would love to visit ${vacationSpots[i]}`)
+}
