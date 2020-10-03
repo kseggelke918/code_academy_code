02bccf62b513654e6791fc22a410c85c2235844e
kseggelke918 <katie.seggelke@gmail.com>
2020-10-03 13:24:34 -0600

    add shorthand to factoryFunctions.js

--- factoryFunctions.js
+++ factoryFunctions.js
@@ -11,3 +11,20 @@ const robotFactory = (model, mobile) => {
   const tinCan = robotFactory('P-500', true)
   
   tinCan.beep()
+
+  // can use shorthand/destructing technique - property value shorthand
+
+  function robotFactory(model, mobile){
+    return {
+      model,
+      mobile,
+      beep() {
+        console.log('Beep Boop');
+      }
+    }
+  }
+  
+  // To check that the property value shorthand technique worked:
+  const newRobot = robotFactory('P-501', false)
+  console.log(newRobot.model)
+  console.log(newRobot.mobile)
