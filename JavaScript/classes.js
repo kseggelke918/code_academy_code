2908a6f4b30fa1dafa7b9bb43ae7cdbc1f31f0d7
kseggelke918 <katie.seggelke@gmail.com>
2020-10-14 12:46:20 -0600

    add another example of getter and method in surgeon class for classes.js

--- JavaScript/classes.js
+++ JavaScript/classes.js
@@ -1,4 +1,4 @@
-// creating instances: 
+// CREATING INSTANCES: 
 
 class Surgeon {
     constructor(name, department) {
@@ -12,3 +12,59 @@ class Surgeon {
   const surgeonDurant = new Surgeon('Durant', 'Orthopedics')
   // new keyword calls the constructor function
   // which instatiates a new Surgeon object
+
+
+// METHODS & GETTERS:
+
+class Dog {
+    constructor(name) {
+      this._name = name;
+      this._behavior = 0;
+      // _ indicates these properties shouldn't be accessed directly
+    }
+  
+    //getter - needs the get keyword
+    get name() {
+      return this._name;
+    }
+  
+    //getter
+    get behavior() {
+      return this._behavior;
+    }
+  
+    //method
+    incrementBehavior() {
+      this._behavior++;
+    }
+  }
+
+  // EXAMPLE OF GETTER AND METHOD:
+
+  class Surgeon {
+    constructor(name, department) {
+      this._name = name;
+      this._department = department;
+      this._remainingVacationDays = 20;
+    }
+  
+    get name() {
+      return this._name
+    }
+  
+    get department() {
+      return this._department
+    }
+  
+    get remainingVacationDays() {
+      return this._remainingVacationDays
+    }
+  
+    takeVacationDays(daysOff) {
+      this._remainingVacationDays = this._remainingVacationDays - daysOff
+      return this._remainingVactionDays
+    }
+  }
+  
+  const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+  const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
