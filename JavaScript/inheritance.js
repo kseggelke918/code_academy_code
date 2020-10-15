a99f0c62d0445a606c9ee18e0aa11bc23e7781ac
kseggelke918 <katie.seggelke@gmail.com>
2020-10-15 11:43:16 -0600

    add notes to inheritance.js

--- JavaScript/inheritance.js
+++ JavaScript/inheritance.js
@@ -19,7 +19,15 @@ class HospitalEmployee {
 
   class Nurse extends HospitalEmployee {
     constructor(name, certifications){
+        // super keyword calls the constructor of the parent class
+        // super(name) passes the name arg from the nurse class to the constructor
+        // of the hospitalEmployee class where it will set the name
+
+        // **** always call super BEFORE using this keyword when setting other properties
+        // otherwise JS will throw a reference error - best practice is to put it
+        // at the top of the constructor
       super(name)
+      // this is a property that is not available in the parent class
       this._certifications = certifications
     }
   }
