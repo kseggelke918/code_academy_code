0b6688bb48237db8b4090d884b9b713fc059322d
kseggelke918 <katie.seggelke@gmail.com>
2020-11-05 14:16:00 -0700

    add compiling.java

---
+++ Java/Compiling.java
@@ -0,0 +1,34 @@
+public class Compiling {
+    public static void main(String[] args) {
+    
+        System.out.println("Java is a class-based language.");
+        System.out.println("Java classes have a 'main' method.");
+        System.out.println("Java statements end with a semicolon.")
+    
+        System.out.println("Programming is... fun!");
+        
+      } 
+}
+
+// javac Compiling.java in the terminal will throw an error and won't 
+// compile b/c we are missing a ; 
+// can check is a .class file was run by checking ls in the terminal
+
+// Java is a compiled language, meaning the code we write in .java file 
+// is transformed into byte code by a compiler before it's executed by the
+// Java Virtual Machine on your computer.
+
+// Compiler: program that transalates human-friendly programming
+// languages into other programming languages that computers can 
+// execute. 
+    // * catches mistakes before computer runs our code
+    // * runs a series of checks while it transforms the code - 
+    // code that doesn't pass these checks won't be compiled.
+
+// COMPILE TERMINAL COMMAND: javac Compiling.java
+// A successful compilation produces a .class file: Compiling.class that 
+// we can execute with TERMINAL COMMAND: java Compiling
+// An unseccessful compilation produces a list of error and non .class
+// file is made until the errors are corrected and the compile command is run again
+
+
