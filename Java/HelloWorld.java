6f358d59b393993292ad4f2d57d78d50e6b0f0df
kseggelke918 <katie.seggelke@gmail.com>
2020-11-05 13:49:02 -0700

    add syntax notes to HelloWorld.java

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,5 +1,13 @@
+// Each file has one primary class named after the file
+// Syntax Note: Every word is capitalized - camel cased
+// Syntax Note: use "" instead of ''
+
+// HelloWorld matches file name HelloWorld.java
 public class HelloWorld {
-    public static void main(String[] args) {
+// the main method lists our program tasks - necessary for program to run
+//String[] args - a placeholder for info we want to pass into our program
+  public static void main(String[] args) {
       System.out.println("Hello World!");
+      // println is short for 'print line'
     }
   }
