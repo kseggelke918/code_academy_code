97f72ee6882d6d1f800b85a8f9a88be776f47c27
kseggelke918 <katie.seggelke@gmail.com>
2020-11-06 14:46:09 -0700

    add example codes and notes to variables.java

--- Java/Variables.java
+++ Java/Variables.java
@@ -1,7 +1,34 @@
 public class Variables {
-    
+    public static void main(String[] args) {
+        String name = "James Gosling";
+        int yearCreated = 1995;
+        System.out.println(name);
+        System.out.println(yearCreated);
+        }
 }
 
 // In Java, we specify the type of information we're storing.  
 // PRIMITIVE DATATYPES are types of data built-in to the Java system.
 
+// MUST DELCARE A VARIABLE TO REFERENCE IT WITHIN OUR PROGRAM
+    // ** requires that we specify the type and name
+    /* 
+        int age; 
+        double salaryRequirement; 
+        booleen isEmployed
+    */ 
+
+    // int, double and boolean are built-in variable types
+    // age, salaryRequirement, isEmployed are the variable names
+    // the variables above don't have values assigned yet
+    // to assign values to a variable, we use the assignment operator =
+
+    /* 
+        age = 85
+    */ 
+
+    // We will commonly declare and assign a variable on one line
+
+    /* 
+    int yearCodeAcademyWasFounded = 2011; 
+    */ 
