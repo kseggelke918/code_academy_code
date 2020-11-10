98f75ad88800366211eb4ae76150f965a4191a55
kseggelke918 <katie.seggelke@gmail.com>
2020-11-10 15:43:18 -0700

    add notes and examples to Boolean.java

--- Java/DataTypes/Boolean.java
+++ Java/DataTypes/Boolean.java
@@ -1,5 +1,10 @@
 package DataTypes;
 
 public class Boolean {
-    
+    public static void main(String[] args) {
+        boolean javaIsACompiledLanguage = true; 
+        boolean javaIsACupOfCoffee = false; 
+    }
 }
+
+// True or False values
