684ffbcff7633277fb29b14c94fc9ddb31fb50b3
kseggelke918 <katie.seggelke@gmail.com>
2020-11-10 15:39:32 -0700

    add notes and examples to double.java

--- Java/DataTypes/doubles.java
+++ Java/DataTypes/doubles.java
@@ -1,5 +1,15 @@
 package DataTypes;
 
 public class doubles {
+public static void main(String[] args){
+    // doubles can have decimals
+    double price = 8.99; 
+    // doubles can have values larger than what int can hold 
+    double gdp = 12237700000;
+}
     
 }
+
+// double can hold decimals as well as very large and small numbers
+// max = 1.797,693,134,862,315,7 E+308, which is approximately 17 followed by 307 zeros.
+// min = 4.9 E-324, which is 324 decimal places
