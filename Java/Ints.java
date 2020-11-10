5d018174321df718d31573035b321575dc7a3b0d
kseggelke918 <katie.seggelke@gmail.com>
2020-11-10 15:23:04 -0700

    add ints.java and add notes about the int data type

---
+++ Java/Ints.java
@@ -0,0 +1,15 @@
+public record Ints() {
+    // int variable declaration
+    int yearJavaWasCreated; 
+    // assignment 
+    yearJavaWasCreated - 1996; 
+    // declaration and assignment 
+    int numberOfPrimitiveTypes = 8;
+}
+
+// ints (integers) are whole numbers - use the int primitive data type
+// hold positive numbers, negative numbers and zero
+// no fractions or decimals
+// allows for numbers between -2,147,483,648 and 2,147,483,647
+
+
