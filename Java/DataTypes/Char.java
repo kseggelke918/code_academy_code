edaeb224438a4204be8272937bf2cdf07e1626e6
kseggelke918 <katie.seggelke@gmail.com>
2020-11-10 15:47:22 -0700

    add char.java file, notes and examples

---
+++ Java/DataTypes/Char.java
@@ -0,0 +1,11 @@
+package DataTypes;
+
+public class Char {
+    public static void main(String[] args) {
+        char grade = 'A';
+        char firstLetter = 'p';
+        char punctionation = '!';
+    }
+}
+// can hold any character like a letter, space, punctionation, etc.
+// MUST BE SURROUNDED BY SINGLE QUOTES ''
