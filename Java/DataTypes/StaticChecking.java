606ac2b4294b12a5c860d3d38806491b262fced6
kseggelke918 <katie.seggelke@gmail.com>
2020-11-10 16:08:31 -0700

    add StaticChecking file and notes

---
+++ Java/DataTypes/StaticChecking.java
@@ -0,0 +1,27 @@
+package DataTypes;
+
+public class StaticChecking {
+    public static void main(String[] args) {
+
+    }
+}
+
+// Java Programming language has statis typing.  Java programs will not
+// compile if a variable is assigned a value of an incorrect type.  This a 
+// bug, specifically a type declaration bug.
+
+// Bugs cause our code to crash or produce incorrect results.  Static 
+// typing helps because bugs are caught during programming rather than
+// during execution of code.
+
+// The program will not compile if the declared type of the variable 
+// does not match the type of the assigned value.  Example: 
+// int greeting = "Hello World"; (string cannot be held in int)
+// Would produce an error at compilation: 
+// error: incompatible types: STring cannot be converted to int
+
+// When bugs aren't caught at compilation, they interrupt execution of the code 
+// by causing runtime errors.  The program will crash.
+
+// Java's static typing helps programmers avoid runtime errors, and 
+// thus have much safer code that is free from bugs.
