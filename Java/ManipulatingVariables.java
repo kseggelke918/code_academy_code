0e46a1b0e9481767a0606348e6c1444b4d954d92
kseggelke918 <katie.seggelke@gmail.com>
2020-11-12 14:50:36 -0700

    add manipulatingVaribales.java, example and notes

---
+++ Java/ManipulatingVariables.java
@@ -0,0 +1,15 @@
+public class ManipulatingVariables {
+    public static void main(String[] args) {
+        // declare initial balance 
+        double balance = 2000.99;
+        // declare deposit amount 
+        double depositAmount = 100.00;
+        // store result of calculation in new variable 
+        double updatedBalance = balance + depositAmount;
+
+        
+    }
+}
+
+//Java has built in math operations that perform calculations on numberic 
+// values
