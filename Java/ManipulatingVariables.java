d9a05df840fefd4f9a166108c5c6beed50c11f29
kseggelke918 <katie.seggelke@gmail.com>
2020-11-12 14:54:06 -0700

    add additional notes to manipulatingvariables

--- Java/ManipulatingVariables.java
+++ Java/ManipulatingVariables.java
@@ -7,9 +7,12 @@ public class ManipulatingVariables {
         // store result of calculation in new variable 
         double updatedBalance = balance + depositAmount;
 
-        
+        // subtracting works the same
+        double withdrawAmount = 500; 
+        double updatedBalance = balance - withdrawAmount;         
     }
 }
 
 //Java has built in math operations that perform calculations on numberic 
 // values
+// math works with int and double
