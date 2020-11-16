a95f80530b0203db886b1d498aeabaa247176505
kseggelke918 <katie.seggelke@gmail.com>
2020-11-16 14:25:27 -0700

    add == and != notes and examples

--- Java/RelationalOperators.java
+++ Java/RelationalOperators.java
@@ -6,10 +6,23 @@ public class RelationalOperators {
         // would print true b/c amountToWithdraw is less than the balance
         // Can also store the true/false in a variable
         boolean enoughFunds = amountToWithdraw < balance;
-        System.out.println(enoughFunds)
+        System.out.println(enoughFunds);
+
+        // Use relational operator == to see if 2 variables are equal
+        double paycheckAmount = 620; 
+        double calculatedPaycheck = 15.50 * 40; 
+        System.out.println(paycheckAmount == calculatedPaycheck);
+        // prints true b/c they are equal
+
+        // to check if 2 variables are not equal, use !=
+        double secondBalance = 20000.0; 
+        double amountToDeposit = 620; 
+        double updatedBalance = secondBalance + amountToDeposit; 
+        boolean balanceHasChanged = balance != updatedBalance;
+        // holds true b/c balance does not equal the updated balance
     }
     
 }
 
 // Java has relational operators for numeric datatypes that 
-// make boolean comparisons < > 
+// make boolean comparisons < > == !=
