184bd52f575e87c38df983efef0bf54e13bfcc26
kseggelke918 <katie.seggelke@gmail.com>
2020-11-16 14:14:32 -0700

    add notes and examples for modulo in manipulating variables

--- Java/ManipulatingVariables.java
+++ Java/ManipulatingVariables.java
@@ -27,6 +27,19 @@ public class ManipulatingVariables {
         int unevenlyDivided = 10 / 4; 
         // INTS CANNOT STORE DECIMALS - DROPS REMAINDER, MAKES RESULT FIT INT TYPE
         // unevenlyDivided = 2 b/c 10 / 4 == 2.5 and the remainder drops
+
+        // Modulo % gies us the remainder after 2 numbers are divided.
+        int cookiesBaked = 10; 
+        int cookiesLeftOver = 10 % 3; 
+        // cookiesLeftOver == 1
+
+        // Can use modulo to see if a number is even or odd
+        // dividing an even number by 2 will have a remainder of 0, odd 
+        // will have a remainder o 1
+        int even = 8 % 2; 
+        // == 0, even
+        int odd = 7 % 2; 
+        // == 1, odd  
     }
 }
 
