d02d45f60a233b2ebd5e63272f0d1c9bf9f01c28
kseggelke918 <katie.seggelke@gmail.com>
2020-11-18 14:30:35 -0700

    add classes.java and notes

---
+++ Java/Classes.java
@@ -0,0 +1,22 @@
+public class Classes {
+    // what should a savings account know? - the balance (Fields/state)
+    // what should a savings account do? - dep and w/d money (Methods/behaviors)
+    
+}
+
+// All programs require one or more classes that act as a model for the world.
+
+// ex) a progrm to track student test scores might have Student, Course, and Grade
+// classes.  Our real-world concerns, students and grades, are inside the progran
+// as classes.  Represent each student as an instance or object of the 
+// Student class.  OBJECT ORIENTATED PROGRAMMING b/c programs are 
+// built around objects and their interations.  An object contains
+// state and behavior
+
+// Methods - behavior 
+// Fields - state
+
+// Classes are a blueprint for objects - detail the general structure
+
+// ex) all studetns have an ID.  THIS student has an ID of 42 and THIS course
+// enrolled THAT student
