1ad7dfcdf125c9ec9897a1c4b3fcb879ec43b45b
kseggelke918 <katie.seggelke@gmail.com>
2020-11-21 14:06:20 -0700

    add ClassSyntax.java file with notes and examples

---
+++ Java/ClassSyntax.java
@@ -0,0 +1,23 @@
+public class ClassSyntax {
+    // scope of ClassSyntax is between {}
+    // 'public' is an access level modifier that allows other classes to interact with this class
+    public static void main(String[] args){
+        // scope of main is between {}
+
+        // program tasks go here
+        // main() lists the tasks to be performed by the program 
+        // main() runs when we execute the compiled ClassSyntax.class fle
+    }
+
+    // out of main, still in ClassSyntax
+    
+}
+
+// out of ClassSyntax scope
+
+// fundamental concept of object-orientated programming is the class
+// class is set of instructions that describe how an instance 
+// can behave and what info it contains
+
+// Java has pre-defined classes such as System (which we've used to log 
+// text to screen System.out.println())
