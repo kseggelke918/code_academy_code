079606a1cf78d62900d51ae874a6f944a37ff7a1
kseggelke918 <katie.seggelke@gmail.com>
2020-11-23 19:26:32 -0700

    add instancevariables.java, add notes and example code

--- Java/Classes/InstanceVariables.java
+++ Java/Classes/InstanceVariables.java
@@ -1,5 +1,33 @@
 package Classes;
 
 public class InstanceVariables {
-    
+    public static void main(String[] args) {
+
+    }
 }
+
+// instance variables / instance fields refers to associated data to an object
+// Instance fields are the state in our objects
+
+// example: 
+/* 
+
+public class Car {
+    // declare fields inside the class by specifying the type and name
+    // the DECLARATION is within the class and the instance variable will be 
+    // available for assignment inside the constructor
+    String color; 
+
+    public Car() {
+        // instance fields available in the scope of the constructor method
+    }
+
+    public static void main(String[] args) {
+        // body of main method
+    }
+}
+
+*/ 
+
+// Fields are a type of state each instance will possess.  One instnce may have
+// color=red and another may be blue, etc.
