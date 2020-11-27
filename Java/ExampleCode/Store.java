12892ddd5a90d673101b974041b8e60e779886b2
kseggelke918 <katie.seggelke@gmail.com>
2020-11-27 14:26:17 -0700

    add example code in Store.java with notes

--- Java/ExampleCode/Store.java
+++ Java/ExampleCode/Store.java
@@ -1,5 +1,27 @@
 package ExampleCode;
 
 public class Store {
-    
+    // instance fields
+    // define variable for constructor
+    String productType; 
+
+    // constructor method 
+    public Store(String product) {
+        productType = product;
+    }
+
+    // regular method
+    public void advertise() {
+        System.out.println("Come spend some money!");
+        System.out.println("Selling " + productType + "!");
+    }
+
+    // main method - always have to have one
+    public static void main(String[] args){
+        // program tasks go here
+        // main() lists the tasks to be performed by the program 
+        // can create instances in the main method
+        // main() runs when we execute the compiled ClassSyntax.class fle
+
+    }
 }
