9009ca4cd284d29475fecf96d2ebe2792df8c645
kseggelke918 <katie.seggelke@gmail.com>
2020-12-01 15:37:29 -0700

    add SavingsAccount.java file and starter example code indluding blank main method, constructor and blank deposit and withdraw methods

---
+++ Java/ExampleCode/SavingsAccount.java
@@ -0,0 +1,23 @@
+package ExampleCode;
+
+public class SavingsAccount {
+    double balance; 
+
+    // constructor 
+    public SavingsAccount(double startingBalance) {
+        balance = startingBalance; 
+    }
+
+    public void deposit(double amountToDeposit){
+        // Add amount to the balance 
+
+    }
+
+    public void withdraw(double amountToWithdraw) {
+        // subtract amountToWithdraw from balance
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
