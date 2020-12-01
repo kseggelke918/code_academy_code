3f6b49a727653907913aa4721aeecec3cf3975c7
kseggelke918 <katie.seggelke@gmail.com>
2020-12-01 15:28:46 -0700

    add code and notes to car.java

--- Java/ExampleCode/Car.java
+++ Java/ExampleCode/Car.java
@@ -1,5 +1,21 @@
 package ExampleCode;
 
 public class Car {
-    
+    String color; 
+
+    // constructor
+    public Car(String carColor){
+        color = carColor;
+    }
+    // method with station as a parameter used in the method
+    public void startRadio(String station) {
+        System.out.println("Turning on the radio to " + station + "!");
+        System.out.println("Enjoy!");
+    }
+
+    // main method - where the program tasks live
+    public static void main(String[] args){
+        Car myCar = new Car("red");
+        myCar.startRadio("Mediation Station");
+    }
 }
