3d7c24d220bb3f0438170141d81e2c4896e83bd3
kseggelke918 <katie.seggelke@gmail.com>
2020-12-02 15:56:56 -0700

    add returns.java with notes and example about the return keyword and defining the method with datatypes of the return

---
+++ Java/Returns.java
@@ -0,0 +1,23 @@
+public class Returns {
+
+    // this method will return 4
+    // instead of using 'void' in method name, we use int to SIGNIFY THE RETURN TYPE OF THE METHOD BEING INT
+    public int numberOfTires(){
+        int tires = 4; 
+        return tires;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
+
+// variables can only exist in the scope they were declared in.
+
+// We can use a value outside of a method it was created in if we RETURN it from the method
+
+// Return a value by using the keyword 'return'
+
+// VOID indicates to the method that no value is returned afer calling the method
+
+// when the method should return a certain datatype, we use those datatype keywords in the titling
+// of the method to specify that a method should return that value type 
