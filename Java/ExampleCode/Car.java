5c97cea069e2a4ef4666d291a7b641241a7e44ec
kseggelke918 <katie.seggelke@gmail.com>
2020-12-02 16:01:12 -0700

    add numberOfTires method and method call in main in car.java example code:

--- Java/ExampleCode/Car.java
+++ Java/ExampleCode/Car.java
@@ -7,7 +7,16 @@ public class Car {
     public Car(String carColor){
         color = carColor;
     }
+
+    // 'int' is used instead of 'void' b/c int gives the datatype that should be returned in this method
+    
+    public int numberOfTires() {
+        int tires = 4; 
+        return tires;
+        // this method will return 4
+    }
     // method with station as a parameter used in the method
+    // using 'void' in the method name means that this method won't have a return value
     public void startRadio(String station) {
         System.out.println("Turning on the radio to " + station + "!");
         System.out.println("Enjoy!");
@@ -16,6 +25,10 @@ public class Car {
     // main method - where the program tasks live
     public static void main(String[] args){
         Car myCar = new Car("red");
+        // since numberOfTire() returns 4, the value 4 is now saved to the variable numTires
+        int numTires = myCar.numberOfTires();
+        System.out.println(numTires); 
+        // would print 4
         myCar.startRadio("Mediation Station");
     }
 }
