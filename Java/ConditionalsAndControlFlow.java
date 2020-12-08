016b82508c0b00c10d55a56c5cea7cfe08eeb020
kseggelke918 <katie.seggelke@gmail.com>
2020-12-08 14:29:52 -0700

    add conditionalsAndControlflow.java with conditional notes and hello world example

---
+++ Java/ConditionalsAndControlFlow.java
@@ -0,0 +1,12 @@
+public class ConditionalsAndControlFlow {
+   public static void main(String[] args) {
+       if(true){
+           System.out.println("Hello World!");
+       }
+   } 
+}
+
+// conditional statements allow us to be selective in which portions of our code will run
+
+// conditional statements check a boolean condition and run a block of code depending on the condition
+// THINK IF/ELSE or if-then statements
