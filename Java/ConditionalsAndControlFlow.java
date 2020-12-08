7dcf6ce6657744b06b0b1f207df2f467dbcc446e
kseggelke918 <katie.seggelke@gmail.com>
2020-12-08 14:36:30 -0700

    add isValidPassword and numberOfItemsInCart conditionals

--- Java/ConditionalsAndControlFlow.java
+++ Java/ConditionalsAndControlFlow.java
@@ -12,6 +12,21 @@ public class ConditionalsAndControlFlow {
            // will only run if what is in () is true (in this case: if flip == 1 is true) 
            System.out.println("Heads!");
        }
+
+       // create a variable with a boolean data type
+       boolean isValidPassword = true; 
+
+       // use the variable assigned above in the conditional
+       if (isValidPassword) {
+           System.out.println("Password Accepted!");
+       }
+
+       int numberOfItemsInCart = 9; 
+
+       // if numberOfItemsInCart is > 12, then the code in the {} will execute, otherwise, nothing will happen
+       if (numberOfItemsInCart > 12){
+           System.out.println("Express checkout not available");
+       }
    } 
 }
 
