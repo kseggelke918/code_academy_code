30a9954192bedf5c021a026e8cccae1ba32d768d
kseggelke918 <katie.seggelke@gmail.com>
2020-12-15 15:37:59 -0700

    add notes about switch statements

--- Java/Conditionals/SwitchStatement.java
+++ Java/Conditionals/SwitchStatement.java
@@ -2,11 +2,20 @@ package Conditionals;
 
 public class SwitchStatement {
     String course = "History";
-    
+
+    // student will be enrolled in History class by checking
+    // the value contained in the parentheses, course, against each of
+    // the case labels -- if the value after the case label matches
+    // the value within the (), the switch block is run
+    // if no matches found - the default case is run
     switch(course) {
         case "Algebra": 
         // enroll in algebra
             break;
+            // break keyword is used to exit the switch statement
+            // w/o break, code below the matching case label is run, 
+            // including code under other case lebels which is rarely 
+            // desired behavior
         case "Biology": 
             // enroll in biology
             break; 
