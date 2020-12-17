435ad39a0b81eaee3d3c1755a6889e17d9b86228
kseggelke918 <katie.seggelke@gmail.com>
2020-12-17 13:27:48 -0700

    add examples of using && for 2 conditions

--- Java/Conditionals/And.java
+++ Java/Conditionals/And.java
@@ -2,7 +2,26 @@ package Conditionals;
 
 public class And {
     // can nest 2 conditionals: 
-    
+    if (tuitionPaid) {
+        if (hasPrerequisite) {
+            // enroll student
+        }
+    }
+    // the above will do the job, but we can be more concise:
+    if (tuitionPaid && hasPrerequisite) {
+        // enroll student
+    }
 }
 
 // when more than one condition must be met
+// && is used between two boolean values and evaluates to a single 
+// boolean value
+
+true && true 
+// true 
+false && true 
+// false 
+true && false 
+// false 
+false && false 
+// false 
