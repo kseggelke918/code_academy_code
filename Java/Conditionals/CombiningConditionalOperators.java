1b1115f5197a17e893e05b820a210aeffb95a128
kseggelke918 <katie.seggelke@gmail.com>
2020-12-30 15:55:29 -0700

    add notes and example to combiningconditionaloperators.java

--- Java/Conditionals/CombiningConditionalOperators.java
+++ Java/Conditionals/CombiningConditionalOperators.java
@@ -3,3 +3,17 @@ package Conditionals;
 public class CombiningConditionalOperators {
     
 }
+
+// Have the ability to expand our boolean expressions by using
+// multiple conditional operators in a single expression
+
+// example: 
+
+boolean foo = true && !(false || !true);
+// foo = true && opposite of (false or false)
+
+// ORDER OF EVALUATION WHEN IT COMES TO CONDITIONALS: 
+// 1. Conditions placed in ()
+// 2. NOT - !
+// 3. AND - && 
+// 4. OR - ||
