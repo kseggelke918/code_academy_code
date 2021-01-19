90fb26a9066ecfe788dee248a7fa1ce8638b1ce6
kseggelke918 <katie.seggelke@gmail.com>
2021-01-19 15:55:58 -0700

    add notes about arrays

--- Java/Arrays/ArrayBasics.java
+++ Java/Arrays/ArrayBasics.java
@@ -3,3 +3,7 @@ package Arrays;
 public class ArrayBasics {
     
 }
+
+// an array holds a fixed number of values of ONE TYPE.  
+// arrays hold doubles, ints, booleans or any other primitives
+// arrays can also contain Strings as well as object references
