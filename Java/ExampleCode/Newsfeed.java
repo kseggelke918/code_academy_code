23a86806f6bc202699ed21c6b8b98db29d08dfee
kseggelke918 <katie.seggelke@gmail.com>
2021-01-19 16:08:46 -0700

    add Newsfeed.java with getTopics method and method call

---
+++ Java/ExampleCode/Newsfeed.java
@@ -0,0 +1,20 @@
+package ExampleCode;
+
+public class Newsfeed {
+    
+    public Newsfeed(){
+
+    }
+
+    public String[] getTopics(){
+        String[] topics = {"Opinion", "Tech", "Science", "Health"};
+        return topics;
+      }
+
+      public static void main(String[] args){
+        Newsfeed sampleFeed = new Newsfeed();
+    
+        String[] topics = sampleFeed.getTopics();
+        System.out.println(topics);
+      }
+}
