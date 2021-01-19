c2d7241ece6d2d29d7aa17418aa724d9130c0081
kseggelke918 <katie.seggelke@gmail.com>
2021-01-19 16:01:31 -0700

    add examples of declaring and initiating arrays

--- Java/Arrays/ArrayBasics.java
+++ Java/Arrays/ArrayBasics.java
@@ -1,9 +1,21 @@
 package Arrays;
 
+
 public class ArrayBasics {
+    // creating an array explicitly: 
+    // first declare the type of data it holds:
+    double[] prices; 
+    // next, explicitly initialize the array to contain the data we want to store:
+    prices = {13.15, 15.87, 14.22, 16.66};
+
+    // can also declare on one line: 
+    double[] prices = {13.15, 15.87, 14.22, 16/66};
+
+    String[] clothingItems = {"Tank Top", "Beanie", "Funny Socks", "Corduroys"};
     
 }
 
 // an array holds a fixed number of values of ONE TYPE.  
 // arrays hold doubles, ints, booleans or any other primitives
 // arrays can also contain Strings as well as object references
+
